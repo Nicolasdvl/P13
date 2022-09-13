a46aa104573f482f61f16f8ab2bb029ba7916f18
Daval Nicolas <nicolasdvl@vivaldi.net>
2022-09-13 16:30:15 +0200

    add 'lang:fr' filter in get_fr_tweets_about()

--- app/twt_api/parser.py
+++ app/twt_api/parser.py
@@ -7,41 +7,30 @@ load_dotenv()
 
 class Parser:
     """
+    Parse data from Twitter API.
 
-    def __init__(self):
-        self.url = "https://api.twitter.com/2/tweets/"
-        self.header = {"key": "APIkey"}
-        self.params = "params"
-
-    def request_api(self, query: str = None) -> str:
-        """ """
-        if query:
-            self.params["query"] = query
-        try:
-            response = requests.get(
-                url=self.url, header=self.header, params=self.params
-            )
-        except requests.exceptions.RequestException as error:
-            raise error
-        return response.json()
-
-     """
+    Work with tweepy package. Parser.client.* to use tweepy method.
+    """
 
     def __init__(self) -> None:
+        """Initialize tweepy.Client."""
         self.client = tweepy.Client(os.environ.get("TWITTER_BEARER"))
-        self.tweet_fields = (
-            "id, author_id, text, lang, created_at, public_metrics"
-        )
+        self.tweet_fields = "id,author_id,text,lang,created_at,public_metrics"
 
-    def get_tweets(
+    def get_fr_tweets_about(
         self, query: str, max_result=10
     ) -> list | tweepy.TweepyException:
-        """Return list of tweets object for a query"""
+        """
+        Return list of tweet object for a query.
+
+        Tweet attr is defined in self.tweet_fields.
+        Query is filter by lang:fr.
+        """
+        query += " lang:fr"
         try:
             response = self.client.search_recent_tweets(
                 query, max_results=max_result, tweet_fields=self.tweet_fields
             )
-            print("HERE", response.errors)
             return response.data
         except tweepy.TweepyException as e:
             return e
