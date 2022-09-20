5b62fd0891a1d286e50213444584ff1f90f9b4e3
Daval Nicolas <nicolasdvl@vivaldi.net>
2022-09-20 15:31:49 +0200

    add conversation in view context

--- app/app_site/views.py
+++ app/app_site/views.py
@@ -5,6 +5,7 @@ from twt_api.parser import Parser
 
 def index(request):
     context = {}
+    conversations = []
     if request.method == "POST":
         query = request.POST.get("q")
         db_tweets = Tweets()
@@ -12,7 +13,20 @@ def index(request):
         tweets = db_tweets.get_tweets_about(query)
         if not tweets:
             tweets = parser.get_fr_tweets_about(query)
+            for tweet in tweets:
+                if conversation := parser.get_conversation(
+                    tweet.conversation_id
+                ):
+                    conversations.append(conversation)
+
+        else:
+            conversations.extend(
+                db_tweets.get_conversation(tweet.conversation_id)
+                for tweet in tweets
+            )
+
         context["tweets"] = tweets
+        context["conversations"] = conversations
         for tweet in tweets:
             db_tweets.insert(tweet)
         return render(request, "index.html", context=context)
