5c835052696d76d3aff43a520fe02b9a08140c81
Daval Nicolas <nicolasdvl@vivaldi.net>
2022-09-30 22:58:31 +0200

    add mock in tests Parser methods

--- app/twt_api/tests/test_parser.py
+++ app/twt_api/tests/test_parser.py
@@ -1,3 +1,4 @@
+from unittest.mock import MagicMock
 from django.test import TestCase
 from ..parser import Parser
 
@@ -5,6 +6,17 @@ from ..parser import Parser
 class ParserTestCase(TestCase):
     def setUp(self) -> None:
         self.parser = Parser()
+        self.parser.get_fr_tweets_about = MagicMock(return_value="tweet")
+        self.parser.get_conversation = MagicMock(return_value="conversation")
 
-    def test_parser(self) -> None:
-        pass
+    def test_get_fr_tweets_about(self) -> None:
+        # Arrange
+        result = self.parser.get_fr_tweets_about("cat")
+        # Assert
+        self.assertEqual(result, "tweet")
+
+    def test_get_conversation(self) -> None:
+        # Arrange
+        result = self.parser.get_conversation(123456789)
+        # Assert
+        self.assertEqual(result, "conversation")
