1208aa36160434ba6ca7556b0e54697138db5b6e
Daval Nicolas <nicolasdvl@vivaldi.net>
2022-10-20 23:02:21 +0200

    modify admin view : fields display and filter

--- app/twt_db/admin.py
+++ app/twt_db/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Tweets, TwitterUser
+from .models import Tweets
 
-admin.site.register(Tweets)
-admin.site.register(TwitterUser)
+
+class TweetsAdmin(admin.ModelAdmin):
+    """Tweets visualisation for admin panel."""
+
+    list_display = (
+        "id",
+        "author_id",
+        "created_at",
+        "conversation_id",
+        "query",
+    )
+    list_display_links = ("id",)
+    list_filter = ("conversation_id", "query")
+    ordering = ("conversation_id",)
+
+    def has_change_permission(self, request, object=None):
+        """Disable edition for Tweets fields"""
+        return False
+
+
+admin.site.register(Tweets, TweetsAdmin)
